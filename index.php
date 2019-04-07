27bedc0bbb6ef51102f9d42e4773709bed180371
Audranp <46485947+Audranp@users.noreply.github.com>
2019-04-07 20:03:01 +0200

    Index 1.1

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <center>
 	<h1> SITE DE LA MAIRIE </h1>
 	<form method ="post" action ="">
-	IDP : <input type ="text" name="idP"> </br> 
+	Pseudo : <input type ="text" name="pseudo"> </br> 
 	MDP : <input type ="password" name="mdp"> </br> 
 	<input type ="reset" name ="Annuler" value ="Annuler">
 	<input type ="submit" name ="SeConnecter" value ="Se Connecter"><br/>
@@ -20,31 +20,28 @@
 	<?php
 		if (isset($_POST['SeConnecter']))
 		{
-			$idP = $_POST['idP'];
+			$pseudo = $_POST['pseudo'];
 			$mdp = $_POST['mdp'];
-			$resultat = verif_connexion ($idP, $mdp);
+			$resultat = verif_connexion ($pseudo, $mdp);
 			//var_dump($resultat);
 			if($resultat==null)
 			{
 				echo"<br/> veuillez vérifier vos identifiants ";
 			}else{
-				echo "<br/> Bienvenue ".$resultat["nom"]."  ".$resultat["prenom"];				
+				echo "<br/> Bienvenue ".$resultat["nom"]." , ".$resultat["prenom"]."";			
 				$_SESSION['nom']=$resultat['nom'];
-				$_SESSION['idP']=$resultat['idP'];
+				$_SESSION['prenom']=$resultat['prenom'];
+				$_SESSION['Idp']=$resultat['Idp'];
+
+
 			}
 		}
-		if (isset($_SESSION['nom']))
+		if (isset($_SESSION['Idp']))
 		{
 						echo "</center>
-		<br/> <a href='index.php?page=1'> Voir les cantines </a>";
-		echo "<br/> <a href='index.php?page=2'> Voir les loisirs </a>";
-	        echo "<br/> <a href='index.php?page=3'> voir evenement </a>";
-                echo "<br/> <a href='index.php?page=4'> Voir les objets en vente </a>";
-                echo "<br/> <a href='index.php?page=5'> Attraper un café en bluetooth</a>";
-                echo "<br/> <a href='index.php?page=6'> Voir les objets en échange </a>";
-                echo "<br/> <a href='index.php?page=7'> Faire une vente d'objet </a>";
-                echo "<br/> <a href='index.php?page=8'> Faire un don d'objet</a>";
-                echo "<br/> <a href='index.php?page=9'> Faire un échange d'objet </a>";
+					<br/> <a href='index.php?page=1'> Cantines </a>";
+				echo "<br/> <a href='index.php?page=2'> Voir les loisirs </a>";
+				 echo "<br/> <a href='index.php?page=3'> Voir evenement </a>";
                 echo "<br/> <a href='index.php?page=10'> Se Déconnecter </a>";
 				
 				if (isset($_GET['page']))
@@ -56,7 +53,6 @@
 				switch ($page)
 				{
 					case 1 :
-						
 					$lesLignes = afficher_cantine();
 					//var_dump($lesLignes);
 					echo "
@@ -68,30 +64,13 @@
 					foreach ($lesLignes as $uneLigne)
 					{
 						echo "<tr> <td>".$uneLigne['ville']."</td>
-						<td>".$uneLigne['codepostal']."</td>
-						<td>".$uneLigne['prix']."</td><tr>";
-					}
-					break;
-						
-					case 2:
-					
-					$lesLignes = afficher_loisir();
-					//var_dump($lesLignes);
-					echo "
-					<table border=1>
-					<tr><td>ID loisir</td><td>Libelle </td> <td>Lieu</td> 
-					";
-					//parcourir les lignes et les afficher dans la table
-					foreach ($lesLignes as $uneLigne)
-					{
-						echo "<tr> <td>".$uneLigne['idL']."</td>
-						<td>".$uneLigne['libelle']."</td>
-						<td>".$uneLigne['lieu']."</td></tr>";
+						<td>".$uneLigne['codepostal']."</td></tr>"
+						.$uneLigne['prix']."</td><tr>";
 					}
 					break;
 					
-						
-					CASE 4:
+
+					case 4:
 					$lesLignes = afficher_loisir();
 					//var_dump($lesLignes);
 					echo "
@@ -102,13 +81,12 @@
 					//parcourir les lignes et les afficher dans la table
 					foreach ($lesLignes as $uneLigne)
 					{
-						echo "<tr> <td>".$uneLigne['idL']."</td>
-						<td>".$uneLigne['libelle']."</td>
-						<td>".$uneLigne['lieu']."</td></tr>";
+						echo "<tr> <td>".$uneLigne['libelle']."</td>
+						<td>".$uneLigne['lieuL']."</td></tr>";
 					}
 					break;
-						
-					case 3:
+
+					case 5:
 					$lesLignes = afficher_evenement();
 					//var_dump($lesLignes);
 					echo "
@@ -122,12 +100,15 @@
 						<td>".$uneLigne['Libelle']."</td></tr>";
 					}
 					break;
-						
+
 					case 6:
 					$lesLignes = afficher_association();
 
 					echo " <table border=1>
-					<tr><td> libelleA </td> <td> adresse </td> <td> tel </td> <td> codeP </td> "
+					<tr><td> libelleA </td>
+					 <td> adresse </td> 
+					 <td> tel </td>
+					 <td> codeP </td> ";
 
 
 					foreach ($lesLignes as $uneLigne)
@@ -138,7 +119,8 @@
 						".$uneLigne['codeP']." </td>
 						<td>";
 					}
-					
+					break;
+
 					case 10: session_destroy();
 				}
 		}
