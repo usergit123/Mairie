44bd46acdd5c2f1faf50e177f7e8b6604629d80c
usergit123 <48957714+usergit123@users.noreply.github.com>
2019-04-16 10:26:04 +0200

    Update index.php
    ajout de l'affichage des mariés

--- index.php
+++ index.php
@@ -120,6 +120,24 @@
 						</tr>";
 					}
 					break;
+					case 5:
+					$lesLignes = afficher_mariage();
+					echo " <table border=1>
+					<tr><td> nom 1er marié(é) </td>
+					 <td> prenom 1er marié(é)</td>
+					 <td> nom 2ème marié(é) </td>
+					 <td> prenom 2ème marié(é) </td> ";
+					foreach ($lesLignes as $uneLigne)
+					{
+						echo "<tr> <td>".$uneLigne['a']." </td>
+						<td> ".$uneLigne['b']." </td>
+						<td>".$uneLigne['c']." </td>
+						<td>".$uneLigne['d']." </td>
+						</tr>";
+					}
+					break;
+					case 10: session_destroy();
+				}
 
 					case 10: session_destroy();
 				}
