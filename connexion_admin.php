b5b2596457dfde4438d9c49aae80d2165b921e22
usergit123 <48957714+usergit123@users.noreply.github.com>
2019-10-09 13:03:08 +0200

    Update connexion_admin.php
    ajouter/supprimer

--- connexion_admin.php
+++ connexion_admin.php
@@ -9,7 +9,7 @@
 <meta name ="viewport" content="width=device-width,initial-scale=1.0"/>
 <link rel="stylesheet" type="text/css" href="./css/style.css">
 <?php 
-if (isset($_POST['supprimer'])) 
+/*if (isset($_POST['supprimer'])) 
 { 
 header('Location: connexion_admin.php'); 
 } 
@@ -18,7 +18,7 @@ if (isset($_POST['ajouter']))
 { 
 header('Location: connexion_admin.php'); 
 } 
-
+*/
 ?>
 </head>
 <body>
@@ -45,7 +45,7 @@ header('Location: connexion_admin.php');
 				echo "<br/> Bienvenue ".$resultat["pseudo"];			
 				$_SESSION['numA']=$resultat['numA'];
 				$int='prenom';
-				//echo strlen((string)$int); 
+				echo strlen((string)$int); 
 				
 			}
 		}
@@ -71,20 +71,71 @@ header('Location: connexion_admin.php');
 				{
 					case 1 :
 					$lesLignes = afficher_cantine();
-					//var_dump($lesLignes);
-					echo "
-					<table border=1>
-					<tr><td>Ville </td> <td>Code postal</td>
-					<td> Prix</td> </tr>
-					";
-					//parcourir les lignes et les afficher dans la table
-					foreach ($lesLignes as $uneLigne)
-					{
-						echo "<tr> <td>".$uneLigne['ville']."</td>
-						<td>".$uneLigne['codepostal']."</td>
-						<td>".$uneLigne['prix']."</td><tr>";
-					}
-					break;
+                    //var_dump($lesLignes);
+                    echo "
+                    <table border=1>
+                    <tr><td>Lieu </td> <td>Libelle</td>
+                    ";
+                    //parcourir les lignes et les afficher dans la table
+                    foreach ($lesLignes as $uneLigne)
+                    {
+                        echo "<tr> <td>".$uneLigne['ville']."</td>
+                        <td>".$uneLigne['codepostal']."</td>
+                        <td>".$uneLigne['prix']."</td> </tr>";
+                    }
+
+                    echo "</table>";
+                    ?>
+                    <h1> Ajouter une cantine </h1>
+                    <form method ="post" action ="">
+                    <tr> Ville : <input type='text' name='ville'> </tr>
+                    <tr> Code postal : <input type='text' name='codepostal'> </tr>
+                    <tr> Prix : <input type='text' name='prix'> </tr>
+                    <input type ="reset" name ="Annuler" value ="Annuler">
+                    <input type ="submit" name ="ajouter" value ="ajouter"><br/>
+                    </form>
+<?php
+                    if(isset($_POST['ajouter']))
+                    {
+
+                         $ville=$_POST['ville'];
+                         $codepostal=$_POST['codepostal'];
+                         $prix=$_POST['prix'];
+
+
+                         $requete="insert into cantine values (null,'".$ville."','".$codepostal."','".$prix."');";
+                         echo $requete;
+                         mysqli_query(connexion(),$requete);
+                    } ?>
+
+                    <h1> Supprimer une cantine </h1>
+                    <form method ="post" action ="">
+                    Cantine à supprimer : 
+                    <td><SELECT name="idC" size="1">
+                    <?php
+                    $connexion = mysqli_connect("localhost","root","","mairie");
+                    $requete="select * from cantine;";
+                    $resultats=mysqli_query($connexion,$requete);
+                    while ($ligne = mysqli_fetch_assoc($resultats))
+                            {
+                                echo"<option value ='".$ligne["idC"]."'>".$ligne["ville"]."</option>";
+                            }
+
+                         ?>
+</SELECT></td>
+                    <input type ="reset" name ="Annuler" value ="Annuler">
+                    <input type ="submit" name ="supprimer" value ="supprimer"><br/>
+                    </form>
+                    <?php
+                    if(isset($_POST['supprimer']))
+                    {
+                         $idC=$_POST['idC'];
+
+                         $requete="delete from cantine where idC=".$idC.";";
+                         mysqli_query(connexion(),$requete);
+                    }
+                    break;
+                   
 					
 					case 2:
 					$lesLignes = afficher_loisir();
@@ -152,50 +203,137 @@ header('Location: connexion_admin.php');
 					}
 					break;
 					case 3:
-					$lesLignes = afficher_evenement();
-					//var_dump($lesLignes);
-					echo "
-					<table border=1>
-					<tr><td>Lieu </td> <td>Libelle</td>
-					";
-					//parcourir les lignes et les afficher dans la table
-					foreach ($lesLignes as $uneLigne)
-					{
-						echo "<tr> <td>".$uneLigne['lieu']."</td>
-						<td>".$uneLigne['libelle']."</td></tr>";
-					}
-					break;
+					 $lesLignes = afficher_evenement();
+                    //var_dump($lesLignes);
+                    echo "
+                    <table border=1>
+                    <tr><td>Lieu </td> <td>Libelle</td>
+                    ";
+                    //parcourir les lignes et les afficher dans la table
+                    foreach ($lesLignes as $uneLigne)
+                    {
+                        echo "<tr> <td>".$uneLigne['lieu']."</td>
+                        <td>".$uneLigne['libelle']."</td></tr>";
+                    }
+
+                    echo "</table>";
+                    ?>
+                    <h1> Ajouter un évènement </h1>
+                    <form method ="post" action ="">
+                    <tr>Lieu de l'évènement : <input type='text' name='lieu'> </tr>
+                    <tr>Nom de l'évènement <input type='text' name='libelle'> </tr>
+                    <input type ="reset" name ="Annuler" value ="Annuler">
+                    <input type ="submit" name ="ajouter" value ="ajouter"><br/>
+                    </form>
+<?php
+                    if(isset($_POST['ajouter']))
+                    {
+
+                         $libelle=$_POST['libelle'];
+                         $lieu=$_POST['lieu'];
+
+                         $requete="insert into evenement values (null,'".$lieu."','".$libelle."');";
+                         echo $requete;
+                         mysqli_query(connexion(),$requete);
+                    } ?>
+
+                    <h1> Supprimer un evenement </h1>
+                    <form method ="post" action ="">
+                    Evènement à supprimer : 
+                    <td><SELECT name="idEV" size="1">
+                    <?php
+                    $connexion = mysqli_connect("localhost","root","","mairie");
+                    $requete="select * from evenement;";
+                    $resultats=mysqli_query($connexion,$requete);
+                    while ($ligne = mysqli_fetch_assoc($resultats))
+                            {
+                                echo"<option value ='".$ligne["idEV"]."'>".$ligne["libelle"]."</option>";
+                            }
+
+                         ?>
+                         </SELECT></td>
+                    <input type ="reset" name ="Annuler" value ="Annuler">
+                    <input type ="submit" name ="supprimer" value ="supprimer"><br/>
+                    </form>
+                    <?php
+                    if(isset($_POST['supprimer']))
+                    {
+                         $idL=$_POST['idEV'];
+
+                         $requete="delete from evenemennt where idEV=".$idEV.";";
+                         mysqli_query(connexion(),$requete);
+                    }
+                    break;
 					case 4:
 					$lesLignes = afficher_association();
-					echo " <table border=1>
-					<tr><td> libelleA </td>
-					 <td> adresse </td> 
-					 <td> tel </td>
-					 <td> codeP </td> ";
-					foreach ($lesLignes as $uneLigne)
-					{
-						echo "<tr> <td>".$uneLigne['libelleA']." </td>
-						<td> ".$uneLigne['adresse']." </td>
-						<td>".$uneLigne['tel']." </td>
-						<td>".$uneLigne['codeP']." </td>
-						</tr>";
-					}break;
-					case 5:
-					$lesLignes = afficher_mariage();
-					echo " <table border=1>
-					<tr><td> nom 1er marié(é) </td>
-					 <td> prenom 1er marié(é)</td>
-					 <td> nom 2ème marié(é) </td>
-					 <td> prenom 2ème marié(é) </td> ";
-					foreach ($lesLignes as $uneLigne)
-					{
-						echo "<tr> <td>".$uneLigne['a']." </td>
-						<td> ".$uneLigne['b']." </td>
-						<td>".$uneLigne['c']." </td>
-						<td>".$uneLigne['d']." </td>
-						</tr>";
-					}
-					break;
+                    //var_dump($lesLignes);
+                    echo "
+                    <table border=1>
+                    <tr><td>Libelle </td> <td>Adresse</td>
+                    <td>Telephone</td><td>Code Postal</td>
+                    ";
+                    //parcourir les lignes et les afficher dans la table
+                    foreach ($lesLignes as $uneLigne)
+                    {
+                        echo "<tr> <td>".$uneLigne['libelleA']."</td>
+                        <td>".$uneLigne['adresse']."</td>
+                        <td>".$uneLigne['tel']."</td>
+                        <td>".$uneLigne['codeP']."</td> </tr>";
+                    }
+
+                    echo "</table>";
+                    ?>
+                    <h1> Ajouter une association </h1>
+                    <form method ="post" action ="">
+                    <tr> Nom de l'association : <input type='text' name='libelleA'> </tr>
+                    <tr> Adresse : <input type='text' name='adresse'> </tr>
+                    <tr> Téléphone : <input type='text' name='tel'> </tr>
+                    <tr> Code postal : <input type='text' name='codep'> </tr>
+                    <input type ="reset" name ="Annuler" value ="Annuler">
+                    <input type ="submit" name ="ajouter" value ="ajouter"><br/>
+                    </form>
+<?php
+                    if(isset($_POST['ajouter']))
+                    {
+
+                         $libelleA=$_POST['libelleA'];
+                         $adresse=$_POST['adresse'];
+                         $tel=$_POST['tel'];
+                         $codep=$_POST['codep'];
+
+
+                         $requete="insert into association values (null,'".$libelleA."','".$adresse."','".$tel."','".$codep."');";
+                         echo $requete;
+                         mysqli_query(connexion(),$requete);
+                    } ?>
+
+                    <h1> Supprimer une association </h1>
+                    <form method ="post" action ="">
+                    Association à supprimer : 
+                    <td><SELECT name="idA" size="1">
+                    <?php
+                    $connexion = mysqli_connect("localhost","root","","mairie");
+                    $requete="select * from association;";
+                    $resultats=mysqli_query($connexion,$requete);
+                    while ($ligne = mysqli_fetch_assoc($resultats))
+                            {
+                                echo"<option value ='".$ligne["idA"]."'>".$ligne["libelleA"]."></option>";
+                            }
+
+                         ?>
+                         </SELECT></td>
+                    <input type ="reset" name ="Annuler" value ="Annuler">
+                    <input type ="submit" name ="supprimer" value ="supprimer"><br/>
+                    </form>
+                    <?php
+                    if(isset($_POST['supprimer']))
+                    {
+                         $idA=$_POST['idA'];
+
+                         $requete="delete from association where idA=".$idA.";";
+                         mysqli_query(connexion(),$requete);
+                    }
+                    break;
 					case 6:
 					$lesLignes = afficher_acte($_SESSION['idP']);
 					echo " <table border=1>
